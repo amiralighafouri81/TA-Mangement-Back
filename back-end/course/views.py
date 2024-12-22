c333f0324896be188ef42a296058f69ea2c1b4e8
amirali ghafouri <amiralighafouri81@gmail.com>
2024-12-22 21:37:58 +0330

    fixed(back): create a filters.py for course app

--- back-end/course/views.py
+++ back-end/course/views.py
@@ -1,21 +1,15 @@
-from django_filters import rest_framework as filters
 from rest_framework.viewsets import ModelViewSet
 from .models import Course
 from .serializers import CourseSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import CourseFilter
 
-
-class CourseFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
-
-    class Meta:
-        model = Course
-        fields = ['name']
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    filter_backends = [filters.DjangoFilterBackend]
-    filterset_class = CourseFilter  # Use the custom FilterSet
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CourseFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
