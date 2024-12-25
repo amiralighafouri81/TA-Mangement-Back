4cefff44d96901f71a5eac3a740d8974db54ab5d
amirali ghafouri <amiralighafouri81@gmail.com>
2024-12-25 17:58:00 +0330

    instructors does not see their own information

--- back-end/faculty/views.py
+++ back-end/faculty/views.py
@@ -5,19 +5,24 @@ from rest_framework.exceptions import PermissionDenied
 from .serializers import StudentSerializer, InstructorSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .pagination import DefaultPagination
-from .filters import InstructorFilter
+from .filters import InstructorFilter, StudentFilter
 
 
 class StudentViewSet(ModelViewSet):
     # queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = StudentFilter
+    pagination_class = DefaultPagination
 
     def get_queryset(self):
         user = self.request.user
 
         if user.is_staff:
             return Student.objects.all()
+        else:
+            raise PermissionDenied("You do not have permission to see this endpoint.")
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -33,6 +38,15 @@ class InstructorViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # Exclude the logged-in instructor's information if the user is an instructor
+        if user.role == 'instructor':
+            return self.queryset.exclude(user=user)
+
+        return self.queryset
 
     def destroy(self, request, *args, **kwargs):
         # Check if the user is staff
