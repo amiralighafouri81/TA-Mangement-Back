58f592aabd06bb6fd2ea4ca492199161ab8f3ddf
amirali ghafouri <amiralighafouri81@gmail.com>
2024-12-25 22:53:00 +0330

    instructors only see students that have request to their course to set as head TA

--- back-end/course/serializers.py
+++ back-end/course/serializers.py
@@ -6,7 +6,7 @@ from request.models import Request
 
 class CourseSerializer(serializers.ModelSerializer):
     head_TA = serializers.PrimaryKeyRelatedField(
-        queryset=Request.objects.filter(status=Request.REQUSET_STATUS_ACCEPTED),  # Filter requests with 'accepted' status
+        queryset=Request.objects.none(),  # Initialize with an empty queryset
         required=False,
         allow_null=True
     )
@@ -15,6 +15,19 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'name', 'semester', 'instructor', 'head_TA']
         read_only_fields = ['id', 'name', 'semester', 'instructor']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        request = self.context.get('request')
+        if request and request.user.is_authenticated and request.user.role == 'instructor':
+            # Fetch the instructor's course based on the course being updated
+            instructor_courses = Course.objects.filter(instructor__user=request.user)
+            if self.instance and self.instance in instructor_courses:
+                # Filter requests for the course with 'accepted' status
+                self.fields['head_TA'].queryset = Request.objects.filter(
+                    course=self.instance,
+                    status=Request.REQUSET_STATUS_ACCEPTED
+                )
 
 
 class SimpleCourseSerializer(serializers.ModelSerializer):
