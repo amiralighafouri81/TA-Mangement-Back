6ade3f0a1f9920a8cc0efe07e7406f3ccdbea34d
amirali ghafouri <amiralighafouri81@gmail.com>
2024-12-27 01:56:30 +0330

    filter based on instructor id in courses page

--- back-end/course/filters.py
+++ back-end/course/filters.py
@@ -1,7 +1,8 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, NumberFilter
 from .models import Course
 
 class CourseFilter(FilterSet):
+    instructor_id = NumberFilter(field_name='instructor', lookup_expr='exact')
     class Meta:
         model = Course
         fields = {
