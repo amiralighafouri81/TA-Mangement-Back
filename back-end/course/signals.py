1db59a703a1a870d2aa3ef8ca3da4be3affaecb8
amirali ghafouri <amiralighafouri81@gmail.com>
2024-12-30 04:40:57 +0330

    modify course signals

--- back-end/course/signals.py
+++ back-end/course/signals.py
@@ -5,18 +5,27 @@ from request.models import Request
 
 @receiver(pre_save, sender=Course)
 def handle_condition_change(sender, instance, **kwargs):
-    # Check if the course condition is being updated and if it's different from the original
+    # Check if the course already exists in the database
     if instance.pk:  # If the course already exists
-        old_course = Course.objects.get(pk=instance.pk)
-        if old_course.condition != instance.condition:  # Condition has changed
-            # Find requests where the student's score no longer meets the new condition
-            requests_to_decline = Request.objects.filter(course=instance, status=Request.REQUSET_STATUS_ACCEPTED)
-            for request in requests_to_decline:
-                if request.score < instance.condition:
-                    request.status = Request.REQUSET_STATUS_DECLINED
-                    request.save()
+        try:
+            old_course = Course.objects.get(pk=instance.pk)
+            # Check if the condition has changed
+            if old_course.condition != instance.condition:
+                # Find requests where the student's score no longer meets the new condition
+                requests_to_decline = Request.objects.filter(
+                    course=instance,
+                    status=Request.REQUSET_STATUS_ACCEPTED
+                )
+                for request in requests_to_decline:
+                    if request.score < instance.condition:
+                        # Update the request status to declined
+                        request.status = Request.REQUSET_STATUS_DECLINED
+                        request.save()
 
-                    # If the student was the head TA, remove them as head TA
-                    if instance.head_TA and instance.head_TA.student == request.student:
-                        instance.head_TA = None
-                        instance.save()
+                        # Remove the student as head TA if necessary
+                        if instance.head_TA and instance.head_TA.student == request.student:
+                            instance.head_TA = None
+                            instance.save()
+        except Course.DoesNotExist:
+            # This should not happen as we checked instance.pk, but it's good to handle it
+            pass
